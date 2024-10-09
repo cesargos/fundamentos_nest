8b7dd64f1dcefaba165a1aaf6cec7c737d9d0155
Cesar Goulart <devcesargoulart@gmail.com>
2024-10-08 22:48:23 -0300

    criando rotas de crud de usuários

---
+++ src/users/users.controller.ts
@@ -0,0 +1,64 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  async list() {
+    return {
+      users: [],
+    };
+  }
+
+  @Get(':id')
+  async getOne(@Param() params) {
+    return {
+      user: {},
+      params,
+    };
+  }
+
+  @Put(':id')
+  async update(@Body() body, @Param() params) {
+    return {
+      user: {},
+      params,
+      body,
+      method: 'put',
+    };
+  }
+
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() params) {
+    return {
+      user: {},
+      params,
+      body,
+      method: 'patch',
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param() params) {
+    return {
+      message: `User ${params?.id} foi deletado com sucesso!`,
+    };
+  }
+
+  @Post()
+  async create(@Body() body) {
+    return {
+      user: {},
+      body,
+      method: 'post',
+    };
+  }
+}
