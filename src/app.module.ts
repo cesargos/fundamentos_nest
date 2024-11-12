8846e53584c98b8178920b4f2fa1f2b765c6a66e
Cesar Goulart <devcesargoulart@gmail.com>
2024-11-11 23:30:22 -0300

    feat: configurando o throttler para proteção de Rate limiting - atk de força

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [forwardRef(() => UsersModule), forwardRef(() => AuthModule)],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 1000,
+        limit: 100,
+      },
+    ]),
+    forwardRef(() => UsersModule),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
